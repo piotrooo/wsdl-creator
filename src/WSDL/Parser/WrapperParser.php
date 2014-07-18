e823333348739a503f6919792507057f0ad56f8c
drivera <drivera@drivera-ThinkPad-T430>
2014-07-18 16:05:04 -0300

    Return types now can have arrays of complex types and complex types inside.

--- src/WSDL/Parser/WrapperParser.php
+++ src/WSDL/Parser/WrapperParser.php
@@ -8,6 +8,8 @@ namespace WSDL\Parser;
 
 use ReflectionClass;
 use ReflectionProperty;
+use WSDL\Types\Arrays;
+use WSDL\Types\Object;
 
 class WrapperParser
 {
@@ -26,24 +28,85 @@ class WrapperParser
     {
         $publicFields = $this->_wrapperClass->getProperties(ReflectionProperty::IS_PUBLIC);
         foreach ($publicFields as $field) {
-            $type = $this->_parseDocForType($field->getDocComment());
-            $this->_makeComplexType($type, $field->getName());
+            $this->_makeComplexType($field->getName(), $field->getDocComment());
         }
     }
 
-    private function _parseDocForType($docComment)
+    private function _makeComplexType($name, $docComment)
     {
-        preg_match('#@type (\w+)#', $docComment, $matches);
-        return trim($matches[1]);
+        if (preg_match('#@type (\w*)\[\]#', $docComment, $matches)) {
+            $type = $matches[1];
+            $strategy = 'array';
+        } else {
+            preg_match('#@type (\w+)#', $docComment, $matches);
+            if (isset($matches[1])) {
+                $type = trim($matches[1]);
+                $strategy = trim($matches[1]);
+            } else {
+                $type = 'void';
+                $strategy = 'void';
+            }
+        }
+
+        switch ($strategy) {
+            case 'object':
+                $this->_complexTypes[] = Object($type, $name, $this->getComplexTypes());
+                break;
+            case 'wrapper':
+                $this->_complexTypes[] = $this->_createWrapperObject($type, $name, $docComment);
+                break;
+            case 'array':
+                $this->_complexTypes[] = $this->_createArrayObject($type, $name, $docComment);
+                break;
+            default:
+                $this->_complexTypes[] = new ComplexTypeParser($type, $name);
+                break;
+        }
+    }
+    
+    private function _createWrapperObject($type, $name, $docComment)
+    {
+        $wrapper = $this->wrapper($type, $docComment);
+        $object = null;
+        if ($wrapper->getComplexTypes()) {
+            $object = new Object($type, $name, $wrapper->getComplexTypes());
+        }
+        return new Object($type, $name, $object);
     }
 
-    private function _makeComplexType($type, $name)
+    private function _createArrayObject($type, $name, $docComment)
     {
-        $this->_complexTypes[] = new ComplexTypeParser($type, $name);
+        $object = null;
+        if ($type == 'wrapper') {
+            $complex = $this->wrapper($type, $docComment)->getComplexTypes();
+            $object = new Object($type, $name, $complex);
+        } else if ($this->isComplex($type)) {
+            $complex = $this->getComplexTypes();
+            $object = new Object($type, $name, $complex);
+        }
+        return new Arrays($type, $name, $object);
     }
 
     public function getComplexTypes()
     {
         return $this->_complexTypes;
+    }
+    
+    public function wrapper(&$type, $docComment)
+    {
+        if (!$this->isComplex($type)) {
+            throw new WrapperParserException("This attribute is not complex type.");
+        }
+        preg_match('#@className=(.*?)(?:\s|$)#', $docComment, $matches);
+        $className = $matches[1];
+        $type = str_replace('\\', '', $className);
+        $wrapperParser = new WrapperParser($className);
+        $wrapperParser->parse();
+        return $wrapperParser;
+    }
+
+    public function isComplex($type)
+    {
+        return in_array($type, array('object', 'wrapper'));
     }
 }
