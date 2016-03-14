a194ba10b632de8caeac7dbe6d58f9c6ea07bd59
Tomas K <iam+git@tomask.info>
2016-03-14 15:21:14 +0100

    Fixed forgotten debugging message when creating type and value

--- src/WSDL/XML/Styles/Style.php
+++ src/WSDL/XML/Styles/Style.php
@@ -69,7 +69,7 @@ abstract class Style
             $type = 'type';
             $value = 'ns:' . 'ArrayOf' . ucfirst($parameter->getName());
         } elseif (TypeHelper::isObject($parameter)) {
-            $type = 'element' . $parameter->getType();
+            $type = 'element';
             $value = 'ns:' . $this->_getObjectName($parameter);
         }
         return array($type, $value);
