141d5d58a364ff89ab465da5a13e50df59d4c143
piotrooo <piotroo89@gmail.com>
2016-04-01 10:09:52 +0200

    try implement ll(1) parser.

--- parser.php
+++ parser.php
@@ -4,7 +4,9 @@
 Grammar:
 
 S -> P
-P -> T R
+P -> T R I
+I -> P
+I -> e
 R -> '[]' N O
 R -> N O
 O -> '{' P '}'
@@ -49,18 +51,21 @@ class Tree
     {
         $type = $this->T();
         list($isArray, $name, $elements) = $this->R();
-        return new Node($type, $name, $isArray, $elements);
+        $nodes = $this->I();
+        $node = new Node($type, $name, $isArray, $elements);
+        $nodes[] = $node;
+        return $nodes;
     }
 
     private function T()
     {
-        return $this->match('/\w+/');
+        return $this->match('\w+');
     }
 
     private function R()
     {
         if ($this->text[$this->position] == '[' && $this->text[$this->position + 1] == ']') {
-            $this->match('/\[\]/');
+            $this->match('\[\]');
             $name = $this->N();
             $elements = $this->O();
             $isArray = true;
@@ -72,28 +77,39 @@ class Tree
         return [$isArray, $name, $elements];
     }
 
-    private function N()
+    private function I()
     {
-        return $this->match('/\$\w+/');
-    }
-
-    private function O()
-    {
-        if (isset($this->text[$this->position]) && $this->match('/\s*\{/')) {
-            $elements = [];
-            $elements[] = $this->P();
-//            while ($this->match('/\s*\}/')) {
-//            }
-            return $elements;
+        if ($this->position < strlen($this->text) && !$this->lookahead('\}')) {
+            return $this->P();
         }
         return [];
     }
 
+    private function N()
+    {
+        return $this->match('\$\w+');
+    }
+
+    private function O()
+    {
+        if (isset($this->text[$this->position]) && $this->match('\{')) {
+            $node = $this->P();
+            $this->match('\}');
+            return $node;
+        }
+        return [];
+    }
+
+    private function lookahead($regexp)
+    {
+        return preg_match('/^\s*' . $regexp . '/', $this->text, $m, 0, $this->position);
+    }
+
     private function match($regexp)
     {
-        if (preg_match($regexp, $this->text, $m, PREG_OFFSET_CAPTURE, $this->position)) {
+        if (preg_match('/^\s*' . $regexp . '/', $this->text, $m, PREG_OFFSET_CAPTURE, $this->position)) {
             $match = $m[0][0];
-            $this->position = $m[0][1] + strlen($match);
+            $this->position = $this->position + strlen($match);
             return $match;
         }
         return null;
@@ -101,9 +117,9 @@ class Tree
 }
 
 $params = [
-    'int $age',
-    'int[] $ages',
-    'object $agent {
+    'int $age
+    int[] $ages
+    object $agent {
         string $name
         int $count
     }',
@@ -115,3 +131,4 @@ foreach ($params as $param) {
     print_r($tree->S());
     echo '===' . PHP_EOL;
 }
+
