516af0f8507e589a18c013a053e292c71015abba
piotrooo <piotroo89@gmail.com>
2016-07-24 17:32:12 +0200

    cleanup.

--- src/WSDL/WSDL.php
+++ src/WSDL/WSDL.php
@@ -1,24 +1,63 @@
 <?php
+/**
+ * Copyright (C) 2013-2016
+ * Piotr Olaszewski <piotroo89@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 namespace WSDL;
 
 use WSDL\Builder\WSDLBuilder;
 use WSDL\XML\XMLProvider;
 
+/**
+ * Class WSDL
+ *
+ * @author Piotr Olaszewski <piotroo89@gmail.com>
+ */
 class WSDL
 {
+    /**
+     * @var string
+     */
     private $xml;
 
-    public function __construct($xml)
+    /**
+     * @param string $xml
+     */
+    private function __construct($xml)
     {
         $this->xml = $xml;
     }
 
+    /**
+     * @return string
+     */
     public function create()
     {
-        header("Content-Type: text/xml");
-        echo $this->xml;
+        return $this->xml;
     }
 
+    /**
+     * @param WSDLBuilder $builder
+     * @return WSDL
+     */
     public static function fromBuilder(WSDLBuilder $builder)
     {
         $xml = new XMLProvider($builder);
